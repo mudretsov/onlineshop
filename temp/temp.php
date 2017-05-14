2ecc0f79f53aa90b5b1ea0638e59337f0533cae0
Maksim <mudrecoff@yandex.ru>
2017-05-14 18:08:04 +0300

    Create 1st version of site

---
+++ temp/temp.php
@@ -0,0 +1,10 @@
+<?php
+$pagetitle='Магазин электроники и компьютернй техники "У Игоря"';
+$menuitem='1';
+require_once '1.php'; /* begin html */
+require_once '3.php'; /* left-menu */
+require 'db_connect.php';
+?>
+
+
+<?php require_once '2.php' ?>
